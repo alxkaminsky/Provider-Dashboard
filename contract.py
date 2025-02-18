ec8fc5dbc02cd52c4b7019f7a17d8d3fc5c64854
Alex Kaminsky <alextron9001@gmail.com>
2025-02-18 15:43:26 -0500

    Add files via upload

---
+++ contract.py
@@ -0,0 +1,264 @@
+"""
+CSC148, Winter 2025
+Assignment 1
+
+This code is provided solely for the personal and private use of
+students taking the CSC148 course at the University of Toronto.
+Copying for purposes other than this use is expressly prohibited.
+All forms of distribution of this code, whether as given or with
+any changes, are expressly prohibited.
+
+All of the files in this directory and all subdirectories are:
+Copyright (c) 2025 Bogdan Simion, Diane Horton, Jacqueline Smith
+"""
+import datetime
+from math import ceil
+from typing import Optional
+from bill import Bill
+from call import Call
+
+# Constants for the month-to-month contract monthly fee and term deposit
+MTM_MONTHLY_FEE = 50.00
+TERM_MONTHLY_FEE = 20.00
+TERM_DEPOSIT = 300.00
+
+# Constants for the included minutes and SMSs in the term contracts (per month)
+TERM_MINS = 100
+
+# Cost per minute and per SMS in the month-to-month contract
+MTM_MINS_COST = 0.05
+
+# Cost per minute and per SMS in the term contract
+TERM_MINS_COST = 0.1
+
+# Cost per minute and per SMS in the prepaid contract
+PREPAID_MINS_COST = 0.025
+
+
+class Contract:
+    """ A contract for a phone line
+
+    This is an abstract class and should not be directly instantiated.
+
+    Only subclasses should be instantiated.
+
+    === Public Attributes ===
+    start:
+         starting date for the contract
+    bill:
+         bill for this contract for the last month of call records loaded from
+         the input dataset
+    """
+    start: datetime.date
+    bill: Optional[Bill]
+
+    def __init__(self, start: datetime.date) -> None:
+        """ Create a new Contract with the <start> date, starts as inactive
+        """
+        self.start = start
+        self.bill = None
+
+    def new_month(self, month: int, year: int, bill: Bill) -> None:
+        """ Advance to a new month in the contract, corresponding to <month> and
+        <year>. This may be the first month of the contract.
+        Store the <bill> argument in this contract and set the appropriate rate
+        per minute and fixed cost.
+
+        DO NOT CHANGE THIS METHOD
+        """
+        raise NotImplementedError
+
+    def bill_call(self, call: Call) -> None:
+        """ Add the <call> to the bill.
+
+        Precondition:
+        - a bill has already been created for the month+year when the <call>
+        was made. In other words, you can safely assume that self.bill has been
+        already advanced to the right month+year.
+        """
+        self.bill.add_billed_minutes(ceil(call.duration / 60.0))
+
+    def cancel_contract(self) -> float:
+        """ Return the amount owed in order to close the phone line associated
+        with this contract.
+
+        Precondition:
+        - a bill has already been created for the month+year when this contract
+        is being cancelled. In other words, you can safely assume that self.bill
+        exists for the right month+year when the cancelation is requested.
+        """
+        self.start = None
+        return self.bill.get_cost()
+
+
+class MTMContract(Contract):
+    """ Contract with no end date, no initial term deposit, and no free minutes.
+    """
+    start: datetime.date
+    bill: Optional[Bill]
+
+    def new_month(self, month: int, year: int, bill: Bill) -> None:
+        """
+        Advance to a new month in the contract, corresponding to <month> and
+        <year>. This may be the first month of the contract.
+        Store the <bill> argument in this contract and set the appropriate rate
+        per minute and fixed cost.
+        """
+        self.bill = bill
+        bill.add_fixed_cost(MTM_MONTHLY_FEE)
+        bill.set_rates("MTM", MTM_MINS_COST)
+
+
+class TermContract(Contract):
+    """Contract with a start and end date, commitment required until end date.
+    Includes an initial large term deposit added to the first month's bill.
+    Early cancellation forfeits the deposit, while completion
+    returns the deposit to the customer.
+
+    === Public Attributes ===
+    end:
+        ending date for the contract, the contract can go past this date.
+    commit:
+        tracks if term commitment period has been completed.
+
+    === Representation Invariants ===
+    end > start
+    """
+    start: datetime.date
+    bill: Optional[Bill]
+    end: datetime.date
+    commit: bool
+
+    def __init__(self, start: datetime.date, end: datetime.date) -> None:
+        """Create a TermContract with an <end> date and
+        <start> date, which starts as inactive.
+        The <commit> parameter is set to False by default.
+        """
+        Contract.__init__(self, start)
+        self.end = end
+        self.commit = False
+
+    def new_month(self, month: int, year: int, bill: Bill) -> None:
+        """Advance to a new month in the contract, corresponding to <month>
+        and <year>.This may be the first month of the contract.
+        Store the <bill> argument in the contract and set the appropriate rate
+        per minute and fixed cost.
+        -If <month> and <year> match the <end> date’s
+        month and year, set <commit> to True.
+        """
+        if self.start.month == month and self.start.year == year:
+            bill.add_fixed_cost(TERM_DEPOSIT)
+
+        if month == self.end.month and year == self.end.year:
+            self.commit = True
+
+        self.bill = bill
+        bill.set_rates("TERM", TERM_MINS_COST)
+        bill.add_fixed_cost(TERM_MONTHLY_FEE)
+
+    def cancel_contract(self) -> float:
+        """
+        Return the amount owed in order to close the phone line associated
+        with this TermContract. Return the deposit to the customer
+        iff <commit> is True. Otherwise, forfeit the deposit.
+
+        Precondition:
+        - a bill has already been created for the month+year when this contract
+        is being cancelled. In other words, you can safely assume that self.bill
+        exists for the right month+year when the cancellation is requested.
+        """
+        cost = self.bill.get_cost()
+        if self.commit:
+            cost -= TERM_DEPOSIT
+        self.start = None
+        self.end = None
+        return cost
+
+    def bill_call(self, call: Call) -> None:
+        """ Add the <call> to the bill.
+
+        Precondition:
+        - a bill has already been created for the month+year when the <call>
+        was made. In other words, you can safely assume that self.bill has been
+        already advanced to the right month+year.
+        """
+        free = TERM_MINS - self.bill.free_min
+        call_duration = ceil(call.duration / 60.0)
+
+        if free > 0:
+            used_free = min(call_duration, free)
+            self.bill.add_free_minutes(used_free)
+            self.bill.add_billed_minutes(call_duration - used_free)
+        else:
+            self.bill.add_billed_minutes(call_duration)
+
+
+class PrepaidContract(Contract):
+    """A pre-paid Contract that has a start date, no end date,
+    and an associated balance.
+
+    === Public Attributes ===
+    balance:
+        A balance on account associated with this contract.
+    """
+    start: datetime.date
+    bill: Optional[Bill]
+    balance: float
+
+    def __init__(self, start: datetime.date, balance: float) -> None:
+        """Create a PrepaidContract with the <start> date and
+        set negative <balance> as initial credit.
+
+        Precondition:
+        - balance >= 0
+        """
+        Contract.__init__(self, start)
+        self.balance = -1 * balance
+
+    def new_month(self, month: int, year: int, bill: Bill) -> None:
+        """Advance to a new month in the contract.
+        This may be the first month of the contract.
+        Store the <bill> argument in this contract and set the appropriate rate
+        per minute along with any carry-over balance.
+
+        -If <balance> has less than a credit of $10, add $25 of credit.
+        """
+        if self.bill:
+            self.balance = self.bill.get_cost()
+        if self.balance > -10:
+            self.balance -= 25
+
+        self.bill = bill
+        bill.add_fixed_cost(self.balance)
+        bill.set_rates("PREPAID", PREPAID_MINS_COST)
+
+    def cancel_contract(self) -> float:
+        """Return the amount owed in order to close the phone line associated
+        with this PrepaidContract.
+
+        -If balance is non-negative, return the amount owed.
+        -If balance is negative, return 0.
+
+        Precondition:
+        - a bill has already been created for the month+year when this contract
+        is being cancelled. In other words, you can safely assume that self.bill
+        exists for the right month+year when the cancellation is requested.
+        """
+        self.start = None
+        cost = self.bill.get_cost()
+        if self.bill.get_cost() < 0:
+            cost = 0
+        self.balance = 0
+        return cost
+
+
+if __name__ == '__main__':
+    import python_ta
+
+    python_ta.check_all(config={
+        'allowed-import-modules': [
+            'python_ta', 'typing', 'datetime', 'bill', 'call', 'math'
+        ],
+        'disable': ['R0902', 'R0913'],
+        'generated-members': 'pygame.*'
+    })
---
+++ filter.py
@@ -0,0 +1,260 @@
+"""
+CSC148, Winter 2025
+Assignment 1
+
+This code is provided solely for the personal and private use of
+students taking the CSC148 course at the University of Toronto.
+Copying for purposes other than this use is expressly prohibited.
+All forms of distribution of this code, whether as given or with
+any changes, are expressly prohibited.
+
+All of the files in this directory and all subdirectories are:
+Copyright (c) 2025 Bogdan Simion, Diane Horton, Jacqueline Smith
+"""
+from call import Call
+from customer import Customer
+
+
+class Filter:
+    """ A class for filtering customer data on some criterion. A filter is
+    applied to a set of calls.
+
+    This is an abstract class. Only subclasses should be instantiated.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def apply(self, customers: list[Customer],
+              data: list[Call],
+              filter_string: str) \
+            -> list[Call]:
+        """ Return a list of all calls from <data>, which match the filter
+        specified in <filter_string>.
+
+        The <filter_string> is provided by the user through the visual prompt,
+        after selecting this filter.
+        The <customers> is a list of all customers from the input dataset.
+
+         If the filter has
+        no effect or the <filter_string> is invalid then return the same calls
+        from the <data> input.
+
+        Note that the order of the output matters, and the output of a filter
+        should have calls ordered in the same manner as they were given, except
+        for calls which have been removed.
+
+        Precondition:
+        - <customers> contains the list of all customers from the input dataset
+        - all calls included in <data> are valid calls from the input dataset
+        """
+        raise NotImplementedError
+
+    def __str__(self) -> str:
+        """ Return a description of this filter to be displayed in the UI menu
+        """
+        raise NotImplementedError
+
+
+class ResetFilter(Filter):
+    """
+    A class for resetting all previously applied filters, if any.
+    """
+
+    def apply(self, customers: list[Customer],
+              data: list[Call],
+              filter_string: str) \
+            -> list[Call]:
+        """ Reset all of the applied filters. Return a List containing all the
+        calls corresponding to <customers>.
+        The <data> and <filter_string> arguments for this type of filter are
+        ignored.
+
+        Precondition:
+        - <customers> contains the list of all customers from the input dataset
+        """
+        filtered_calls = []
+        for c in customers:
+            customer_history = c.get_history()
+            # only take outgoing calls, we don't want to include calls twice
+            filtered_calls.extend(customer_history[0])
+        return filtered_calls
+
+    def __str__(self) -> str:
+        """ Return a description of this filter to be displayed in the UI menu
+        """
+        return "Reset all of the filters applied so far, if any"
+
+
+class CustomerFilter(Filter):
+    """
+    A class for selecting only the calls from a given customer.
+    """
+
+    def apply(self, customers: list[Customer],
+              data: list[Call],
+              filter_string: str) \
+            -> list[Call]:
+        """ Return a list of all unique calls from <data> made or
+        received by the customer with the id specified in <filter_string>.
+
+        The <customers> list contains all customers from the input dataset.
+
+        The filter string is valid if and only if it contains a valid
+        customer ID.
+        - If the filter string is invalid, return the original list <data>
+        - If the filter string is invalid, your code must not crash, as
+        specified in the handout.
+
+        Do not mutate any of the function arguments!
+        """
+        try:
+            for c in customers:
+                if int(filter_string.strip()) == c.get_id():
+                    nums = c.get_phone_numbers()
+                    lst = [call for call in data if
+                           call.src_number in nums or call.dst_number in nums]
+                    return lst
+            raise ValueError
+        except ValueError:
+            return data
+
+    def __str__(self) -> str:
+        """ Return a description of this filter to be displayed in the UI menu
+        """
+        return "Filter events based on customer ID"
+
+
+class DurationFilter(Filter):
+    """
+    A class for selecting only the calls lasting either over or under a
+    specified duration.
+    """
+
+    def apply(self, customers: list[Customer],
+              data: list[Call],
+              filter_string: str) \
+            -> list[Call]:
+        """ Return a list of all unique calls from <data> with a duration
+        of under or over the time indicated in the <filter_string>.
+
+        The <customers> list contains all customers from the input dataset.
+
+        The filter string is valid if and only if it contains the following
+        input format: either "Lxxx" or "Gxxx", indicating to filter calls less
+        than xxx or greater than xxx seconds, respectively.
+        - If the filter string is invalid, return the original list <data>
+        - If the filter string is invalid, your code must not crash, as
+        specified in the handout.
+
+        Do not mutate any of the function arguments!
+        """
+        try:
+            lst = []
+
+            if len(filter_string) != 4:
+                raise ValueError
+
+            condition = filter_string[0]
+            dur = int(filter_string[1:])
+
+            for c in data:
+                if condition == 'L':
+                    if c.duration < dur:
+                        lst.append(c)
+                elif condition == 'G':
+                    if c.duration > dur:
+                        lst.append(c)
+            return lst
+        except ValueError:
+            return data
+
+    def __str__(self) -> str:
+        """ Return a description of this filter to be displayed in the UI menu
+        """
+        return "Filter calls based on duration; " \
+               "L### returns calls less than specified length, G### for greater"
+
+
+class LocationFilter(Filter):
+    """
+    A class for selecting only the calls that took place within a specific area
+    """
+
+    def apply(self, customers: list[Customer],
+              data: list[Call],
+              filter_string: str) \
+            -> list[Call]:
+        """ Return a list of all unique calls from <data>, which took
+        place within a location specified by the <filter_string>
+        (at least the source or the destination of the event was
+        in the range of coordinates from the <filter_string>).
+
+        The <customers> list contains all customers from the input dataset.
+
+        The filter string is valid if and only if it contains four valid
+        coordinates within the map boundaries.
+        These coordinates represent the location of the lower left corner
+        and the upper right corner of the search location rectangle,
+        as 2 pairs of longitude/latitude coordinates, each separated by
+        a comma and a space:
+          lowerLong, lowerLat, upperLong, upperLat
+        Calls that fall exactly on the boundary of this rectangle are
+        considered a match as well.
+        - If the filter string is invalid, return the original list <data>
+        - If the filter string is invalid, your code must not crash, as
+        specified in the handout.
+        """
+        lat_min, lat_max = 43.576959, 43.799568
+        long_min, long_max = -79.697878, -79.196382
+
+        try:
+            locs = filter_string.split(',')
+            tracker = []
+            lst = []
+
+            if len(locs) != 4:
+                raise ValueError
+
+            for loc in locs:
+                lst.append(float(loc.strip()))
+
+            low = (lst[0], lst[1])
+            up = (lst[2], lst[3])
+
+            if not (long_min <= low[0] <= long_max
+                    and long_min <= up[0] <= long_max
+                    and lat_min <= low[1] <= lat_max
+                    and lat_min <= up[1] <= lat_max):
+                raise ValueError
+
+            for c in data:
+                if ((low[0] <= c.dst_loc[0] <= up[0])
+                        and (low[1] <= c.dst_loc[1] <= up[1])
+                        or (low[0] <= c.src_loc[0] <= up[0])
+                        and (low[1] <= c.src_loc[1] <= up[1])):
+                    tracker.append(c)
+            return tracker
+        except ValueError:
+            return data
+
+    def __str__(self) -> str:
+        """ Return a description of this filter to be displayed in the UI menu
+        """
+        return "Filter calls made or received in a given rectangular area. " \
+               "Format: \"lowerLong, lowerLat, " \
+               "upperLong, upperLat\" (e.g., -79.6, 43.6, -79.3, 43.7)"
+
+
+if __name__ == '__main__':
+    import python_ta
+
+    python_ta.check_all(config={
+        'allowed-import-modules': [
+            'python_ta', 'typing', 'time', 'datetime', 'call', 'customer'
+        ],
+        'max-nested-blocks': 4,
+        'allowed-io': ['apply', '__str__'],
+        'disable': ['W0611', 'W0703'],
+        'generated-members': 'pygame.*'
+    })
